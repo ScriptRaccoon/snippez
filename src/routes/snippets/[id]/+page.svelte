b2f820c3a50cbee146d07f6b8a2b279eda81db30
Script Raccoon <scriptraccoon@gmail.com>
2026-02-15 23:51:46 +0100

    copy buttons

--- src/routes/snippets/[id]/+page.svelte
+++ src/routes/snippets/[id]/+page.svelte
@@ -1,7 +1,28 @@
 <script lang="ts">
-	import { Globe, Lock, Eye } from 'lucide-svelte'
+	import { page } from '$app/state'
+	import { Globe, Lock, Eye, CheckCheck } from 'lucide-svelte'
 
 	let { data } = $props()
+
+	let copied_code = $state(false)
+	let copied_url = $state(false)
+
+	async function copy_code() {
+		await navigator.clipboard.writeText(data.snippet.content)
+		copied_code = true
+		setTimeout(() => {
+			copied_code = false
+		}, 2000)
+	}
+
+	async function copy_url() {
+		const url = `${page.url.href}`
+		await navigator.clipboard.writeText(url)
+		copied_url = true
+		setTimeout(() => {
+			copied_url = false
+		}, 2000)
+	}
 </script>
 
 <header>
@@ -28,11 +49,26 @@
 
 <pre class="code-block">{data.snippet.content}</pre>
 
-{#if data.is_owner}
-	<p>
-		<a class="button" href="/snippets/{data.snippet.id}/edit">Edit</a>
-	</p>
-{/if}
+<div class="actions">
+	<button class="button" onclick={copy_url}>
+		{#if copied_url}
+			Copied URL <CheckCheck size={18} />
+		{:else}
+			Copy URL
+		{/if}
+	</button>
+	<button class="button" onclick={copy_code}>
+		{#if copied_code}
+			Copied code <CheckCheck size={18} />
+		{:else}
+			Copy code
+		{/if}
+	</button>
+
+	{#if data.is_owner}
+		<a class="edit-link button" href="/snippets/{data.snippet.id}/edit">Edit</a>
+	{/if}
+</div>
 
 <style>
 	header {
@@ -59,11 +95,21 @@
 	}
 
 	.code-block {
-		margin-block: 2rem;
+		margin-block: 1rem 2rem;
 		overflow-x: auto;
 		background-color: var(--code-bg-color);
 		outline: 1px solid var(--outline-color);
 		padding: 0.5rem;
 		border-radius: 0.25rem;
+	}
+
+	.actions {
+		display: flex;
+		flex-wrap: wrap;
+		gap: 0.5rem;
+	}
+
+	.edit-link {
+		margin-left: auto;
 	}
 </style>
